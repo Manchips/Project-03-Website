4d74ff40d3886e9ae2a7154729ca1d3c95ba1703
bryan <bfowles@csumb.edu>
2022-04-10 22:56:39 -0700

    added springboot and thymeleaf template to the project

---
+++ src/test/java/com/example/project03website/Project03WebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project03website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project03WebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
