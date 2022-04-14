75ca6b334cbecce4c736c4f2ae6d0342b471026d
bryan <bfowles@csumb.edu>
2022-04-14 15:22:11 -0700

    added an index controller that is connected to the landing page //localhost:8080

---
+++ src/main/java/com/example/project03website/backend/IndexController.java
@@ -0,0 +1,12 @@
+package com.example.project03website.backend;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String landingPage(Model model) {
+        return "landingpage";
+    }
+}
