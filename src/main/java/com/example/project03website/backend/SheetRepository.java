332d22212b541285e21690d5f00d4e8fbc2863d0
bryan <bfowles@csumb.edu>
2022-04-21 17:00:25 -0700

    created a sheet class and prepared the sheet repository, currently the repository is not connected

---
+++ src/main/java/com/example/project03website/backend/SheetRepository.java
@@ -0,0 +1,7 @@
+package com.example.project03website.backend;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SheetRepository extends CrudRepository<Sheet,Integer>{
+
+}
